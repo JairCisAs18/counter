ebb0ad33cc556552b1aea4b2d617d364e3f3dbeb
JairCisAs18 <a308860@alumnos.uaslp.mx>
2024-05-13 16:26:47 -0600

    Update

--- counter.py
+++ counter.py
@@ -10,12 +10,26 @@ root = Tk()
 root.title('Contador de piezas')
 root.geometry('600x600+450+50')
 
+btn = Button(root, text='Iniciar', width=180, font=('Arial', 18, 'bold'), compound='top', pady=12)
+btn.place(x=200, y=350)
+close_btn = Button(root, text='Terminar', width=180, font=('Arial', 18, 'bold'), compound='top', pady=12)
+icon = PhotoImage(file=r'icon.png')
+cancel = PhotoImage(file=r'close.png')
+edit_icon = PhotoImage(file=r'pencil.png')
+edit_icon = edit_icon.subsample(9, 9)
+edit_btn = Button(root, width=30, image=edit_icon, borderwidth=3)
+edit_btn.place(x=500, y=10)
+icon = icon.subsample(6, 6)
+cancel = cancel.subsample(6,6)
+
 def show_info():
+    btn.place_forget()
+    close_btn.place(x=200, y=350)
     display = win.EnumDisplayMonitors()[1][2]
     x = display[0]
     y = display[1]
     screen_width = display[2] - x
-    screen_height = display[3] - y - 50
+    screen_height = display[3] - y- 80
     x_separation = screen_width/3
     y_separation = screen_height/5
     window = Toplevel()
@@ -25,7 +39,13 @@ def show_info():
         time = dt.datetime.now()
         time_label.config(text=f'{time:%H:%M:%S}')
         canvas.after(1000, set_time)
+    def hide_info():
+        close_btn.place_forget()
+        btn.place(x=200, y=350)
+        window.destroy()
+        window.update()
     #window.attributes('-fullscreen', True)
+    close_btn.config(image=cancel, command=hide_info)
     window.geometry(f'{screen_width}x{screen_height}+{x}+{y}')
     canvas = Canvas(window, width=screen_width, height=screen_height, bg='black')
     canvas.pack()
@@ -53,15 +73,20 @@ def show_info():
     Label (canvas, text='PROD TIME', font=('Arial', 35, 'bold'), bg='black', fg='white').place(x=x_separation*2+20, y=y_separation*2+25)
     Label (canvas, text='SHIFT', font=('Arial' , 35, 'bold'), bg='black', fg='white').place(x=x_separation*2.15, y=y_separation*2.5+25)
     Label (canvas, text='TAKT TIME', font=('Arial', 35, 'bold'), bg='black', fg='white').place(x=x_separation*2+25, y=y_separation*3+25)
-    Label (canvas, text='REJECT QTY.', font=('Arial', 30, 'bold'), bg='black', fg='white').place(x=x_separation*2+20, y=y_separation*3.5+25)
-    Label (canvas, text='REJECT RATE', font=('Arial', 30, 'bold'), bg='black', fg='white').place(x=x_separation*2+20, y=y_separation*4+25)
-    Label (canvas, text='LENGTH', font=('Arial', 35, 'bold'), bg='black', fg='white').place(x=x_separation*2.1, y=y_separation*4.5+10)
+    Label (canvas, text='REJECT RATE', font=('Arial', 30, 'bold'), bg='black', fg='white').place(x=x_separation*2+20, y=y_separation*3.5+25)
+    Label (canvas, text='LENGTH', font=('Arial', 35, 'bold'), bg='black', fg='white').place(x=x_separation*2.1, y=y_separation*4+25)
     Label (canvas, text='600', font=('Arial', 80, 'bold'), bg='black', fg='cyan').place(x=x_separation*1.35, y=y_separation/5)
     Label (canvas, text='740', font=('Arial', 80, 'bold'), bg='black', fg='cyan').place(x=x_separation*1.35, y=y_separation*1.2)
     Label (canvas, text='5', font=('Arial', 80, 'bold'), bg='black', fg='lime green').place(x=x_separation*1.45, y=y_separation*2.2)
     Label (canvas, text='756', font=('Arial', 80, 'bold'), bg='black', fg='cyan').place(x=x_separation*1.35, y=y_separation*3.2)
     Label (canvas, text='10.5%', font=('Arial', 80, 'bold'), bg='black', fg='red').place(x=x_separation*1.25, y=y_separation*4.2)
     Label (canvas, text=f'{date:%d/%m/%Y}', font=('Arial', 30, 'bold'), bg='black', fg='yellow').place(x=x_separation*2.5+50, y=25)
+    Label (canvas, text='R16-B074', font=('Arial', 30, 'bold'), bg='black', fg='green3').place(x=x_separation*2.5+55, y=y_separation+25)
+    Label (canvas, text='12', font=('Arial', 30, 'bold'), bg='black', fg='green3').place(x=x_separation*2.7, y=y_separation*1.5+25)
+    Label (canvas, text='07:00 - 16:25', font=('Arial', 30, 'bold'), bg='black', fg='green3').place(x=x_separation*2.5+40, y=y_separation*2+25)
+    Label (canvas, text='1', font=('Arial', 30, 'bold'), bg='black', fg='green3').place(x=x_separation*2.7+10, y=y_separation*2.5+25)
+    Label (canvas, text='45.00', font=('Arial', 30, 'bold'), bg='black', fg='cyan').place(x=x_separation*2.66, y=y_separation*3+25)
+    Label (canvas, text='1 ppm', font=('Arial', 30, 'bold'), bg='black', fg='red').place(x=x_separation*2.65, y=y_separation*3.5+25)
     time_label = Label (canvas, text='', font=('Arial', 30, 'bold'), bg='black', fg='yellow')
     set_time()
     time_label.place(x=x_separation*2.6, y=y_separation*0.5+25)
@@ -71,6 +96,32 @@ def show_info():
     # Button(canvas, text='prueba', command=press).place(x=50, y=50)
     # label_test.place(x=x_separation*2.1, y=y_separation*4.5+10)
     #canvas.create_line(0, y_separation*5, screen_width, y_separation*5, fill='white', width=2)
+
+def settings():
+    set = Toplevel(root)
+    set.title('Configuración')
+    set.geometry('550x700+480+20')
+    headers = ['MODEL_N:', 'RNUMBER:', 'PROCESS_N:', 'LINE:', 'REJECT RATE:', 'TAKT TIME:']
+    accept = PhotoImage(file=r'arrow.png')
+    accept = accept.subsample(5, 5)
+    reject = PhotoImage(file=r'cross.png')
+    reject = reject.subsample(5, 5)
+    dist = 20
+    for label in headers:
+        Label(set, text=label, font=('Arial', 18, 'bold')).place(x=30, y=dist)
+        dist += 60
+    model_field = Entry(set, font=('Arial', 18, 'bold'), width=18, borderwidth=2)
+    model_field.place(x=250, y=20)
+    r_field = Entry(set, font=('Arial', 18, 'bold'), width=18, borderwidth=2)
+    r_field.place(x=250, y=80)
+    process_field = Entry(set, font=('Arial', 18, 'bold'), width=18, borderwidth=2)
+    process_field.place(x=250, y=140)
+    line_field = Entry(set, font=('Arial', 18, 'bold'), width=18, borderwidth=2)
+    line_field.place(x=250, y=200)
+    reject_field = Entry(set, font=('Arial', 18, 'bold'), width=18, borderwidth=2)
+    reject_field.place(x=250, y=260)
+    takt_field = Entry(set, font=('Arial', 18, 'bold'), width=18, borderwidth=2)
+    takt_field.place(x=250, y=320)
 
 model_label = Label(root, text='Modelo:', font=('Arial', 18, 'bold'))
 model_label.place(x=15, y=10)
@@ -106,9 +157,7 @@ end_field = Entry(root)
 end_field.config(width=18, font=('Arial', 14, 'bold'), borderwidth=3)
 end_field.place(x=214, y=240)
 
-icon = PhotoImage(file=r'icon.png')
-icon = icon.subsample(6, 6)
-btn = Button(root, text='Iniciar', width=180, font=('Arial', 18, 'bold'), image=icon, compound='top', pady=12, command=show_info)
-btn.place(x=200, y=350)
+btn.config(image=icon, command=show_info)
+edit_btn.config(command=settings)
 
 root.mainloop()
